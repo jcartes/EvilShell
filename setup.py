caf6b8e79521bd36ee8d17371194d4aa854e8ffa
NoctisAtrae <46224745+NoctisAtrae@users.noreply.github.com>
2019-09-08 15:16:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Setup.py EvilShell
+# Noctis Atrae, ver.0.1.5
+
+import os
+import sys
+
+print("""setup.py va installer tout les modules nécessaires à l'éxecution d'EvilShell. Voici la liste de ce(s) module(s) :
+- Termcolor
+""")
+while True:
+  choix = input("Êtes-vous d'accord (O/n) ?: ")
+  if choix == "O" or "o":
+    os.system("python3 -m pip install termcolor")
+  elif choix == "N" or "n":
+    sys.exit("OK ! Au revoir !")
